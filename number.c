3dbbb4144c4975541198146a6fecec5d38825afa
vicky121998 <36404884+vicky121998@users.noreply.github.com>
2018-09-10 06:57:33 +0530

    Add files via upload

---
+++ number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("Enter the number");
+scanf("%d",&n);
+if(n>0)
+printf("Positive");
+elseif(n==0)
+printf("Zero");
+else
+
+printf("Negative");
+}
