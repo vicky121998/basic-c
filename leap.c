11a02085586f1624ef8ad1796bb48281825d4cad
vicky121998 <36404884+vicky121998@users.noreply.github.com>
2018-10-02 09:36:43 +0530

    Create leap.c

---
+++ leap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+scanf("%d",&n);
+	if(n%400==0)
+	printf("yes");
+	else if(n%100==0)
+	printf("no");
+	else if(n%4==0)
+	printf("yes");
+	else
+	printf("no");
+	return 0;
+}
