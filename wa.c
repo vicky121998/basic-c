8b2e99d20cf424018bfe8ea8dc487679cb198957
vicky121998 <36404884+vicky121998@users.noreply.github.com>
2018-10-03 15:38:22 +0530

    Update wa.c

--- wa.c
+++ wa.c
@@ -4,11 +4,15 @@ int main(void) {
 int n,k,i,sum=0;
 	scanf("%d%d",&n,&k);
 	
+	for(i=1;i<=n;i++)
+	{
+		printf("\t%d",i);
+	}
 		for(n=1;n<=k;n++)
 		{
 			sum=sum+n;
 		
 		}
-		printf("%d",sum);
+		printf("\n%d",sum);
 	return 0;
 }
